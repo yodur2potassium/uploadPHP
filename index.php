1ee4d3947d8e75466aab89b951ed5e9b221187d3
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2015-12-02 09:37:30 +0100

    Made html base

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>DropBoite</title>
   </head>
   <body>
+    <h1>Upload tes beaux fichiers sur mon joli serveur</h1>
+    <hr>
+    <form class="" action="action.php" method="post" enctype="multipart/form-data">
+      <fieldset><legend>Fichiers</legend>
+        <input type="hidden" name="name" value="">
+        <label for="">Nom : <input type="text" name="name" value=""></label>
+        <input type="file" name="name" value=""></br>
+        <input type="submit" name="" value="Envoyer">
 
+      </fieldset>
+
+    </form>
   </body>
 </html>
